c574cad49eb1c2d343bf4eabed0b7a93ae545ee2
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-04-06 18:54:11 +0530

    third commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className="container mx-auto flex px-5 pt-24 pb-20 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">Smart 
-            <br className="inline-block"/>Consulting<br className="inline-block"/>Group
+            <br className="md:inline-block lg:inline-block"/>Consulting<br className=" md:inline-block lg:inline-block"/>Group
             </h1>
             <p className="mb-8 leading-relaxed">We helping client  to create technology that uses Blockchain, AI and IOT with our talented expert.</p>
             <div className="flex justify-center">
