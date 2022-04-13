cf8a6bc571ad446a3f83ef63dc831ddf1febbecb
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-04-13 23:19:57 +0530

    changes-13-04

--- src/components/About.js
+++ src/components/About.js
@@ -1,220 +1,210 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const About = () => {
-
-  let MidData = [
-    {
-    id:1,
-    name:'Expert Members',
-    description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatibus ducimus aliquid sit, repudiandae molestias voluptate maxime consectetur aut repellat.',
-    },
-    {
-    id:2,
-    name:'Sequre & Trusted',
-    description:'Our founders Dustin Moskovitz and Justin quis Rosenstein met while leading Engineering team at Facebook.',
-    },
-    {
-    id:3,
-    name:'Expert Members',
-    description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error eius facilis, quos doloribus incidunt architecto sequi perspiciatis ad dicta. Quisquam dicta repellat modi beatae dolores eveniet sed blanditiis eligendi voluptas.',
-    },
-    {
-    id:4,
-    name:'Big Resources',
-    description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio accusamus necessitatibus atque. Cupiditate voluptates, alias praesentium labore iste libero in quo amet itaque earum laudantium.',
-    },
-
-  ]
-
-  let Sdata = [
-    {
-      id:1,
-      name:"3.5K",
-      description:"Projects Done",
-    },
-    {
-      id:2,
-      name:"20million",
-      description:"Plus pages Build",
-    },
-    {
-      id:3,
-      name:"3000+",
-      description:"Device Invented",
-    },
-
-  ]
-
+function About() {
+  //  const s  = [false , false , false , false ];
   const [showText, setShowText] = useState([false, false, false, false]);
   const [showAdd, showNotAdd] = useState(false);
 
   return (
     <>
-       {/* <!-- About us --> */}
-
-       <section className="flex flex-col md:flex-row lg:flex-row p-8 md:gap-4 lg:gap-0  items-center lg:justify-evenly md:justify-evenly md:items-start">
-
-            <div className="  text_start  flex-col flex md:flex-col lg:flex-col  overflow-y-auto  ">
-
-                <div>
-                    <p className=" text-slate-400 text-xl">
-                        Why Choose us
-                    </p>
-                </div>
-                <div className="">
-                    <div>
-                        <h1 className="font-bold head text-5xl md:text-6xl">
-                            We solve your <br />Problem.
-                        </h1>
-                    </div>
-                    <div>
-                        <img className="w-8/12 md:10/12 lg:11/12 block_iot  " src={process.env.PUBLIC_URL + "/imgs/Rectangle.png"} alt="" />
-                    </div>
-                </div>
-
-
+      <section className="flex flex-col md:flex-row lg:flex-row p-8 md:gap-4 lg:gap-0  items-center lg:justify-evenly md:justify-evenly md:items-start">
+        <div className="  text_start  flex-col flex md:flex-col lg:flex-col  overflow-y-auto  ">
+          <div>
+            <p className=" text-slate-400 text-xl">Why Choose us</p>
+          </div>
+          <div className="">
+            <div>
+              <h1 className="font-bold head text-5xl md:text-6xl">
+                We solve your <br />
+                Problem.
+              </h1>
             </div>
-
-            <div className=" pt-8 md:flex md:flex-col text_start">
-                <h1 className="font-s  sub_heading text-2xl lg:text-3xl md:text-3xl">
-                    We don’t sell technology, We<br /> give ideas that boosts your<br /> journey.
-                </h1>
-                        <div className="">
-                            <ul>
-                                <li className="py-2 pt-8">
-                                    <div className="flex justify-between">
-                                        <div>
-                                            <h3 className="hover:cursor-pointer " id="heading_1" >Expert Members </h3>
-                                        </div>
-                                        <div className='cursor-pointer' onClick={() => setShowText([!showText[0], false, false, false])}>
-                                            <span className="flex-row pb-4" id="span_1"> {showText[0] ? "-" : "+"}</span>
-                                        </div>
-                                    </div>
-
-
-
-                                    {
-
-                                        showText[0] &&
-                                        <div className={`transition-all ease-in-out duration-500 overflow-hidden ${showText[0]?'h-auto text-xl opacity-1':'h-0 p-0 text-sm opacity-0'}`}>
-
-                                            <p id="para_1" className="text-sm ">
-                                                Lorem ipsum dolor sit, amet consectetur<br /> adipisicing elit. Vero facere veritatis
-                                                culpa quae<br />
-                                                eum, libero expedita velit dolor accusamus<br /> aliquid.
-                                            </p>
-                                        </div>
-
-                                    }
-
-
-                                </li>
-                                <li className="py-2">
-                                    <div className="flex pt-4 justify-between">
-                                        <div>
-                                            <h3 className="hover:cursor-pointer " id="heading_2">Secure & Trsuted
-                                            </h3>
-                                        </div>
-                                        <div className='cursor-pointer' onClick={() => setShowText([false, !showText[1], false, false])}>
-                                            <span className="flex-row pb-4 " id="span_1"> {showText[1] ? "-" : "+"}</span>
-                                        </div>
-                                    </div>
-
-                                    {
-
-                                        showText[1] &&
-                                        <div className=" animate-fade-in-down">
-                                            <p id="para_1" className="text-sm  ">
-                                                Lorem ipsum dolor sit, amet consectetur<br /> adipisicing elit. Vero facere veritatis
-                                                culpa quae<br />
-                                                eum, libero expedita velit dolor accusamus<br /> aliquid.
-                                            </p>
-                                        </div>
-                                    }
-
-                                </li>
-                                <li className="py-2">
-                                    <div className="flex pt-4 justify-between">
-                                        <div>
-                                            <h3 className="hover:cursor-pointer " id="heading_3" >Expert Members </h3>
-                                        </div>
-
-                                        <div className='cursor-pointer' onClick={() => setShowText([false, false, !showText[2], false])}>
-                                            <span className="flex-row pb-4" id="span_1"> {showText[2] ? "-" : "+"}</span>
-                                        </div>
-                                    </div>
-
-
-                                    {
-                                        showText[2] &&
-
-                                        <div className="animate-fade-in-down">
-
-                                            <p id="para_1" className="text-sm">
-                                                Lorem ipsum dolor sit, amet consectetur<br /> adipisicing elit. Vero facere veritatis
-                                                culpa quae<br />
-                                                eum, libero expedita velit dolor accusamus<br /> aliquid.
-                                            </p>
-                                        </div>
-
-                                    }
-
-                                </li>
-
-                                <li className="py-2">
-                                    <div className="flex pt-4 justify-between">
-                                        <div>
-
-
-                                            <h3 className="hover:cursor-pointer" id="heading_4" >Big Resoruces</h3>
-                                        </div>
-                                        <div className='cursor-pointer' onClick={() => setShowText([false, false, false, (!showText[3])])}>
-
-                                            <span className="flex-row pb-4" id="span_1"> {showText[3] ? "-" : "+"}</span>
-                                        </div>
-
-                                    </div>
-
-
-                                    {
-
-                                        showText[3] &&
-                                        <div className="h-auto animate-fade-in-down">
-
-                                            <p id="para_1" className="text-sm ">
-                                                Lorem ipsum dolor sit, amet consectetur<br /> adipisicing elit. Vero facere veritatis
-                                                culpa quae<br />
-                                                eum, libero expedita velit dolor accusamus<br /> aliquid.
-                                            </p>
-
-                                        </div>
-                                    }
-
-                                </li>
-                            </ul>
-                        </div>
+            <div>
+              <img
+                className="w-8/12 md:10/12 lg:11/12 block_iot  "
+                src="/imgs/Rectangle.png"
+                alt=""
+              />
             </div>
+          </div>
+        </div>
+        <div className=" pt-8 md:flex md:flex-col text_start">
+          <h1 className="font-s  sub_heading text-2xl lg:text-3xl md:text-3xl">
+            We don’t sell technology, We
+            <br /> give ideas that boosts your
+            <br /> journey.
+          </h1>
 
+          <div className="">
+            <ul>
+              <li className="py-2 pt-8">
+                <div className="flex justify-between">
+                  <div>
+                    <h3>Expert Members </h3>
+                  </div>
+                  <div>
+                    <span
+                      className="flex-row pb-4 cursor-pointer"
+                      id="span_1"
+                      onClick={() =>
+                        setShowText([!showText[0], false, false, false])
+                      }
+                    >
+                      {" "}
+                      {showText[0] ? "-" : "+"}
+                    </span>
+                  </div>
+                </div>
 
-        </section>
-        
-            {/* <!-- statistics --> */}
-            <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto ">
-                <div className="flex flex-wrap  text-center justify-center">
-                {
-                  Sdata.map((val)=>(
-                    <div className="p-4 sm:w-1/4 w-1/2" key={val.id}>
-                    <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">{val.name}</h2>
-                    <p className="leading-relaxed">{val.description}</p>
-                    </div>
-                  ))
-                }
+                <div
+                  className={`block p-2 space-y-2 transition-all ease duration-500 overflow-hidden ${
+                    showText[0]
+                      ? "h-auto text-xl opacity-1 "
+                      : "h-0 p-0 text-sm opacity-0"
+                  }`}
+                >
+                  <p id="para_1" className="text-sm ">
+                    Lorem ipsum dolor sit, amet consectetur
+                    <br /> adipisicing elit. Vero facere veritatis culpa quae
+                    <br />
+                    eum, libero expedita velit dolor accusamus
+                    <br /> aliquid.
+                  </p>
                 </div>
+              </li>
+              <li className="py-2">
+                <div className="flex pt-4 justify-between">
+                  <div>
+                    <h3>Secure & Trsuted</h3>
+                  </div>
+                  <div>
+                    <span
+                      className="flex-row pb-4 cursor-pointer"
+                      id="span_1"
+                      onClick={() =>
+                        setShowText([false, !showText[1], false, false])
+                      }
+                    >
+                      {" "}
+                      {showText[1] ? "-" : "+"}
+                    </span>
+                  </div>
                 </div>
-            </section>
+
+                <div
+                  className={`block p-2 space-y-2 transition-all ease duration-500 overflow-hidden ${
+                    showText[1]
+                      ? "h-auto text-xl opacity-1 "
+                      : "h-0 p-0 text-sm opacity-0"
+                  }`}
+                >
+                  <p id="para_1" className="text-sm  ">
+                    Lorem ipsum dolor sit, amet consectetur
+                    <br /> adipisicing elit. Vero facere veritatis culpa quae
+                    <br />
+                    eum, libero expedita velit dolor accusamus
+                    <br /> aliquid.
+                  </p>
+                </div>
+              </li>
+              <li className="py-2">
+                <div className="flex pt-4 justify-between">
+                  <div>
+                    <h3>Expert Members </h3>
+                  </div>
+
+                  <div>
+                    <span
+                      className="flex-row pb-4 cursor-pointer"
+                      id="span_1"
+                      onClick={() =>
+                        setShowText([false, false, !showText[2], false])
+                      }
+                    >
+                      {" "}
+                      {showText[2] ? "-" : "+"}
+                    </span>
+                  </div>
+                </div>
+
+                <div
+                  className={`block p-2 space-y-2 transition-all ease duration-500 overflow-hidden ${
+                    showText[2]
+                      ? "h-auto text-xl opacity-1 "
+                      : "h-0 p-0 text-sm opacity-0"
+                  }`}
+                >
+                  <p id="para_1" className="text-sm">
+                    Lorem ipsum dolor sit, amet consectetur
+                    <br /> adipisicing elit. Vero facere veritatis culpa quae
+                    <br />
+                    eum, libero expedita velit dolor accusamus
+                    <br /> aliquid.
+                  </p>
+                </div>
+              </li>
+
+              <li className="py-2">
+                <div className="flex pt-4 justify-between">
+                  <div>
+                    <h3>Big Resoruces</h3>
+                  </div>
+                  <div>
+                    <span
+                      className="flex-row pb-4 cursor-pointer"
+                      id="span_1"
+                      onClick={() =>
+                        setShowText([false, false, false, !showText[3]])
+                      }
+                    >
+                      {" "}
+                      {showText[3] ? "-" : "+"}
+                    </span>
+                  </div>
+                </div>
+
+                <div
+                  className={`block p-2 space-y-2 transition-all ease duration-500 overflow-hidden ${
+                    showText[3]
+                      ? "h-auto text-xl opacity-1 "
+                      : "h-0 p-0 text-sm opacity-0"
+                  }`}
+                >
+                  <p id="para_1" className="text-sm ">
+                    Lorem ipsum dolor sit, amet consectetur
+                    <br /> adipisicing elit. Vero facere veritatis culpa quae
+                    <br />
+                    eum, libero expedita velit dolor accusamus
+                    <br /> aliquid.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <div className="flex flex-col lg:flex-row md:flex-row items-center gap-2 pt-8 justify-around pb-4">
+        <div></div>
+
+        <div className="py-4">
+          <h1 className="text-4xl font-bold ">3.5K</h1>
+          <p className="text-slate-400">Projects Done</p>
+        </div>
+
+        <div className="py-4">
+          <h1 className="text-4xl font-bold">20million</h1>
+          <p className="text-slate-400">Plus pages build</p>
+        </div>
+        <div className="py-4">
+          <h1 className="text-4xl font-bold">3000+</h1>
+          <p className="text-slate-400">Device invented</p>
+        </div>
+
+        <hr className="bg-black" />
+      </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -1,136 +1,70 @@
-import React,{useState} from 'react'
-// import React, { Component } from "react";
-import Slider from "react-slick";
+import React, { useRef } from "react";
 
+import Story from "./Story";
+import "../Css/OurStory.css";
 
-function OurStory() {
+const OurStory = () => {
+  const ref = useRef();
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
+  const stories = [
+    {
+      pic: "imgs/mask1.jpeg",
+      title: "Best selling plugin.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+    {
+      pic: "imgs/grey.jpg",
+      title: "Best Service Provider.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+    {
+      pic: "imgs/dg.jpg",
+      title: "Highly Train Expert.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+    {
+      pic: "imgs/grey.jpg",
+      title: "WP Award winner.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+    {
+      pic: "imgs/dg.jpg",
+      title: "Best Service Provider.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+    {
+      pic: "imgs/grey.jpg",
+      title: "Highly Train Expert.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+    {
+      pic: "imgs/dg.jpg",
+      title: "WP Award winner.",
+      subtitle: "Lorem ipsum dolor sit amet, consu adielit, sed do eiusmod",
+    },
+  ];
+  return (
+    <div className="ourAchivements">
+      <div className="top">
+        <div className="heading">
+          <h1>Our Success Story.</h1>
+        </div>
+        <div className="slidebtns">
+          <button onClick={() => scroll(-390)}>&larr;</button>
+          <button onClick={() => scroll(390)}>&rarr;</button>
+        </div>
+      </div>
+      <div ref={ref} className="stories">
+        {stories.map((e, i) => (
+          <div key={i}>
+            <Story e={e} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    const slider = React.useRef(null);
-
-
-    var settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
-    let [open,setOpen]=useState([false,false,false,false]);
-    return (
-
-
-        <section >
-
-            <div className="py-16 md:py-16 ">
-
-                <div className="flex sm:flex-row md:flex-row lg:flex-row justify-around">
-                    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-start justify-start pb-1">
-                        Our Success <br />Story.
-                    </h1>
-
-                    <div className=" flex">
-                        <button className="p-2" onClick={() => slider?.current?.slickPrev()}><img src={process.env.PUBLIC_URL + './imgs/left.svg'} className="w-10"></img></button>
-                        <button className="p-2" onClick={() => slider?.current?.slickNext()} ><img src={process.env.PUBLIC_URL + './imgs/right.svg'} className="w-14"></img></button>
-
-                    </div>
-                </div>
-
-            </div>
-
-            <div className="">
-        
-                <Slider ref={slider} {...settings}>
-
-                            <div className="content_box  grad cursor-pointer" onClick={()=>setOpen([!open[0],false,false,false,false])}>
-                                 <img src={process.env.PUBLIC_URL + "./imgs/mask1.jpeg"} className="back " />
-                                <div className={`transit bg-blue-700 mx-12 transition-all ease-in-out duration-500 overflow-hidden ${open[0]?  'h-auto text-xl opacity-1':'h-0 p-0 text-sm opacity-0'}`}>
-                                    <h1 className="text-white lg:text-3xl md:text-2xl text-2xl text-left mx-5 font-bold " >
-                                        Best Service <br />Provider.
-                                     </h1>
-                                    <p className="pt-4 px-2  text-white ">
-                                        Lorem ipsum dolor sit amet, consuadielit, sed do eiusmod
-                                    </p>
-                                </div>
-                            </div> 
-                                <div className="content_box  grad cursor-pointer" onClick={()=>setOpen([false,!open[1],false,false,false])}>
-                                <img src={process.env.PUBLIC_URL + "./imgs/grey.jpg"} className="back " />
-                                    <div className={`transit bg-blue-700 mx-12 transition-all ease-in-out duration-500 overflow-hidden ${open[1]?  'h-auto text-xl opacity-1':'h-0 p-0 text-sm opacity-0'}`}>
-                                        <h1 className="text-white lg:text-3xl md:text-2xl text-2xl text-left mx-5 font-bold " >
-                                        Best Service <br />Provider.
-                                        </h1>
-                                        <p className="pt-4 px-2  text-white ">
-                                        Lorem ipsum dolor sit amet, consuadielit, sed do eiusmod
-                                        </p>
-                                    </div>
-                                </div> 
-                                <div className="content_box  grad cursor-pointer" onClick={()=>setOpen([false,false,!open[2],false,false])}>
-                                <img src={process.env.PUBLIC_URL + "./imgs/dg.jpg"} className="back " />
-                                    <div className={`transit bg-blue-700 mx-12 transition-all ease-in-out duration-500 overflow-hidden ${open[2]?  'h-auto text-xl opacity-1':'h-0 p-0 text-sm opacity-0'}`}>
-                                        <h1 className="text-white lg:text-3xl md:text-2xl text-2xl text-left mx-5 font-bold " >
-                                        Wp Award <br />Winner.
-                                        </h1>
-                                        <p className="pt-4 px-2  text-white ">
-                                        Lorem ipsum dolor sit amet, consuadielit, sed do eiusmod
-                                        </p>
-                                    </div>
-                                </div> 
-                                <div className="content_box  grad cursor-pointer" onClick={()=>setOpen([false,false,false,!open[3],false])}>
-                                <img src={process.env.PUBLIC_URL + "./imgs/grey.jpg"} className="back " />
-                                    <div className={`transit bg-blue-700 mx-12 transition-all ease-in-out duration-500 overflow-hidden ${open[3]?  'h-auto text-xl opacity-1':'h-0 p-0 text-sm opacity-0'}`}>
-                                        <h1 className="text-white lg:text-3xl md:text-2xl text-2xl text-left mx-5 font-bold " >
-                                        Best Service <br />Provider.
-                                        </h1>
-                                        <p className="pt-4 px-2  text-white ">
-                                        Lorem ipsum dolor sit amet, consuadielit, sed do eiusmod
-                                        </p>
-                                    </div>
-                                </div> 
-                                <div className="content_box  grad cursor-pointer" onClick={()=>setOpen([false,false,false,false,!open[4]])}>
-                                <img src={process.env.PUBLIC_URL + "./imgs/dg.jpg"} className="back" />
-                                    <div className={`transit bg-blue-700 mx-12 transition-all ease-in-out duration-500 overflow-hidden ${open[4]?  'h-auto text-xl opacity-1':'h-0 p-0 text-sm opacity-0'}`}>
-                                        <h1 className="text-white lg:text-3xl md:text-2xl text-2xl text-left mx-5 font-bold " >
-                                        Best Service <br />Provider.
-                                        </h1>
-                                        <p className="pt-4 px-2  text-white ">
-                                        Lorem ipsum dolor sit amet, consuadielit, sed do eiusmod
-                                        </p>
-                                    </div>
-                                </div> 
-                      
-                     
-                </Slider>
-            </div>
-        </section>
-
-    )
-}
-
-export default OurStory
+export default OurStory;
